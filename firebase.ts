b064de1ea4962cdd61b303b98df08b8bb47ac13c
Aryan-AAP <pardeshiaryanon@gmail.com>
2024-08-07 20:37:38 +0530

    done

--- firebase.ts
+++ firebase.ts
@@ -1,25 +1,18 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getFirestore } from "firebase/firestore/lite";
-
-import { getStorage } from "firebase/storage"
-
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDo7VKzf2MciMOD1drU6mQRE2N7GaXSnfk",
-    authDomain: "chat-with-ai-98fea.firebaseapp.com",
-    projectId: "chat-with-ai-98fea",
-    storageBucket: "chat-with-ai-98fea.appspot.com",
-    messagingSenderId: "323188877508",
-    appId: "1:323188877508:web:44fc47db75bb151a7dc191"
-  };
-  
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-  const app =getApps().length===0?initializeApp(firebaseConfig):getApp();
-
-  const db =getFirestore(app) 
-
-  const storage = getStorage(app)
-
-  export {db,storage}
+export { db, storage };
